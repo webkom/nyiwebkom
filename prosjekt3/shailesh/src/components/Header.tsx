6738b30ccd8b2010199c570b2d7d59f16fbed65a
ShaileshS1702 <shsha001@outlook.com>
2023-10-24 19:03:38 +0200

    Shailesh sin todo

---
+++ prosjekt3/shailesh/src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+import "./Header.css"
+
+const Header = () => {
+
+
+    return (
+        <div className="header" >
+            <h1>To do list</h1>
+        </div>
+
+    );
+
+}
+
+export default Header;
