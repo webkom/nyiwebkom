9db0db1614c3585911682e6136bb84b824dad20e
Aronbr <aron.reinen@gmail.com>
2025-09-23 18:38:07 +0200

    Startet på nettside

---
+++ opplaering2025/prosjekt1/aron/main.js
@@ -0,0 +1,28 @@
+let newX = 0, newY = 0, startX = 0, startY = 0;
+
+const card = document.getElementById("kort")
+
+card.addEventListener("mousedown", mouseDown)
+
+function mouseDown(e){
+    startX = e.clientX
+    startY = e.clientY
+
+    document.addEventListener("mousemove", mouseMove)
+    document.addEventListener("mouseup", mouseUp)
+}
+
+function mouseMove(e){
+    newX = startX - e.clientX
+    newY = startY - e.clientY
+
+    startX = e.clientX
+    startY = e.clientY
+
+    card.style.top = (card.offsetTop - newY) + "px"
+    card.style.left = (card.offsetLeft - newX) + "px"
+}
+
+function mouseUp(e){
+    document.removeEventListener("mousemove", mouseMove)
+}
